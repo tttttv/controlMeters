f68deca1d6fe6c4171f255044f143be15e587ba9
tttttv <Vlapvlapvlap1>
2023-10-29 01:44:41 +0300

    first

--- web/controlMeters/urls.py
+++ web/controlMeters/urls.py
@@ -21,5 +21,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("landing.urls")),
+    path("", include("ERP_CORE.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
