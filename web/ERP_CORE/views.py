f1f472253470b294b04e9929e041dc06b07ae5f9
tttttv <Vlapvlapvlap1>
2023-10-29 03:06:08 +0300

    first

--- web/ERP_CORE/views.py
+++ web/ERP_CORE/views.py
@@ -16,9 +16,25 @@ def index_view(request):
     }
     return render(request, 'index.html', params)
 
+def unmarshal(self, body, pl):
+    if self.json:
+        return Parse(body, pl)
+
+    pl.ParseFromString(body)
+    return pl
 
 def chirpstack_callback_view(request):
     print(request.POST)
     print(request.GET)
     print(request.body)
+
+    
+    body = request.body
+    try:
+        up = unmarshal(body, integration.UplinkEvent())
+        print("Uplink received from: %s with payload: %s" % (up.device_info.dev_eui, up.data.hex()))
+    except:
+        join = unmarshal(body, integration.JoinEvent())
+        print("Device: %s joined with DevAddr: %s" % (join.device_info.dev_eui, join.dev_addr))
+
     return HttpResponse(status=200)
