933619f815aef5846eaf22907be13563ce0502a5
tttttv <Vlapvlapvlap1>
2023-11-02 22:39:24 +0300

    first

--- web/ERP_CORE/urls.py
+++ web/ERP_CORE/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.index_view, name="index"),
     path("chirpstack", views.chirpstack_callback_view, name="chirpstack"),
-    path("meter/{int:pk}", views.meter_profile_view, name="meter_profile"),
+    path("meter/<int:pk>", views.meter_profile_view, name="meter_profile"),
 ]
