99c7cf41e8b2795f56e23534f4451f522ae3c164
tttttv <Vlapvlapvlap1>
2023-11-02 22:13:23 +0300

    first

--- web/ERP_CORE/admin.py
+++ web/ERP_CORE/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from ERP_CORE.models import WaterMeter, MeterData
+
+admin.site.register(WaterMeter)
+admin.site.register(MeterData)
